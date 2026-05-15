dc5069ec8fc871034de0b92024ea47829d31d25c
Otju <ottoj.soderman@gmail.com>
2026-05-15 15:11:38 +0300

    Improve bracket rendering

--- src/pages/brackets.js
+++ src/pages/brackets.js
@@ -3,14 +3,14 @@ import React from "react";
 const Brackets = () => {
   return (
     <div className="container min-h-[100vh]">
-      <h1 className="text-3xl md:text-5xl font-outfit-bold uppercase pl-10 pt-10 mb-8">
+      <h1 className="text-3xl md:text-5xl font-outfit-bold uppercase pl-10 pt-10 absolute ">
         OSM 2026 Brackets
       </h1>
 
       <iframe
         src="https://aaltogamers.fi/tournaments/osm-2026?stream&textColor=%23e8fefb&teamNameColor=%23302a4b&loseScoreColor=%236EA4BF&winScoreColor=%23FAA916&roundColor=%23211e36&connectorColor=%23FAA916&dividerColor=%23331E36&titleFontSize=30&basicFontSize=24&teamHeight=36&teamWidth=200&teamGapX=20"
         title="OSM 2026 Brackets"
-        className="w-[100vw] h-[100vh] pl-10 pb-10 pt-30"
+        className="w-[100vw] h-[100vh] pl-10 pb-10 pt-28"
       ></iframe>
     </div>
   );
